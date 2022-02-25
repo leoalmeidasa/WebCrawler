6d062399f33c1b03c3d94757e6fa005ef0f299e9
Leonardo <leoalmeidasa@gmail.com>
2022-02-25 11:18:44 -0300

    fix: env production

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class AuthenticationController < ApplicationController
 
-      before_action :auth_user
+      skip_before_action :auth_user
 
       def create
         @user = User.find_by(login: user_params[:login])
